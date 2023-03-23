5e5c6b22c5c046683bfc933fbb32c60794fcc545
Vicky Feng <vickyfeng@dynamic-oit-vapornet100-10-9-189-219.princeton.edu>
2023-03-23 16:54:50 -0400

    process every audio and textgrid

--- data_processing.py
+++ data_processing.py
@@ -3,52 +3,57 @@ import librosa
 import os
 from pydub import AudioSegment
 
-audio_path = "/Users/vickyfeng/Desktop/spice/VF19A_Cantonese_I2_20181114.wav"
-textgrid_path = "//Users/vickyfeng/Desktop/spice/VF19A_Cantonese_I2_20181114.TextGrid"
+# audio_path = "/Users/vickyfeng/Desktop/spice/VF19A_Cantonese_I2_20181114.wav"
+# textgrid_path = "//Users/vickyfeng/Desktop/spice/VF19A_Cantonese_I2_20181114.TextGrid"
 
-audio = AudioSegment.from_wav(audio_path)
-grid = textgrids.TextGrid(textgrid_path)
+audio_ids = {"VF19A_Cantonese_I2_20181114"}
 
-# keep track of which phones belong in which interval
-spice = []
-p_end = 0.0
-p_index = 0
-p_len = len(grid["phone"])
+for id in audio_ids:
+  audio_path = "/scratch/network/vyfeng/spice_data/" + id + ".wav"
+  textgrid_path = "/scratch/network/vyfeng/spice_data/" + id + ".wav"
 
-dir_name = audio_path[-31:-26] 
+  audio = AudioSegment.from_wav(audio_path)
+  grid = textgrids.TextGrid(textgrid_path)
 
-if not os.path.exists(dir_name):
-  os.makedirs(dir_name)
+  # keep track of which phones belong in which interval
+  spice = []
+  p_end = 0.0
+  p_index = 0
+  p_len = len(grid["phone"])
 
-for interval in grid["utterance"]:
-  if interval.text == "":
-    continue
-  text = ""
+  dir_name = audio_path[-31:-26] 
 
-  # extract the audio and sample at 16Hz
-  interval_audio = audio[interval.xmin*1000:interval.xmax*1000]
-  interval_name = "./" + dir_name + "/" + str(interval.xmin)
-  interval_audio.export(interval_name, format="wav")
+  if not os.path.exists(dir_name):
+    os.makedirs(dir_name)
 
-  resampled = librosa.load(interval_name, sr=16000)
+  for interval in grid["utterance"]:
+    if interval.text == "":
+      continue
+    text = ""
 
-  # accumulate the phones that belong in this interval
-  while p_end <= interval.xmax:
-    phone = grid["phone"][p_index].text
+    # extract the audio and sample at 16Hz
+    interval_audio = audio[interval.xmin*1000:interval.xmax*1000]
+    interval_name = "./" + dir_name + "/" + str(interval.xmin)
+    interval_audio.export(interval_name, format="wav")
 
-    if phone == "sp":
-      text += " "
-    else:
-      text += phone
+    resampled = librosa.load(interval_name, sr=16000)
 
-    p_index += 1
+    # accumulate the phones that belong in this interval
+    while p_end <= interval.xmax:
+      phone = grid["phone"][p_index].text
 
-    if p_index >= p_len:
-      break
-    p_end = grid["phone"][p_index].xmax
-    
-  utterance = {"path": audio_path, "audio": {"path": interval_name, "array": resampled, "sampling_rate": 16000}, "phones": text, "sentence": interval.text}
-  spice.append(utterance)
-  break
+      if phone == "sp":
+        text += " "
+      else:
+        text += phone
 
-print(resampled)
+      p_index += 1
+
+      if p_index >= p_len:
+        break
+      p_end = grid["phone"][p_index].xmax
+      
+    utterance = {"path": audio_path, "audio": {"path": interval_name, "array": resampled, "sampling_rate": 16000}, "phones": text, "sentence": interval.text}
+    spice.append(utterance)
+    break
+  print(resampled)
